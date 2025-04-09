28c480e3fa94d0ccdcf1943b52b97fa09de3d596
Pedro Dias Mendes Vianna <ordepmeta@gmail.com>
2025-04-09 13:09:18 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,22 +13,22 @@ function envioDados(dados) {
 
         if (imc < 18.5){
             console.log("Você está abaixo do peso");
-            resultado.innerHTML += `<h1> Você está abaixo do peso ele é ${imc.toFixed(2)}</h1>`;
+            resultado.innerHTML += `<h1> Você está abaixo do peso ele é: <br> ${imc.toFixed(2)}</h1>`;
         } else if (imc >= 18.5 && imc <= 24.9) {
             console.log("Você tem um peso normal");
-            resultado.innerHTML +=`<h1>Você tem um peso normal ele é ${imc.toFixed(2)}</h1>`;
+            resultado.innerHTML +=`<h1>Você tem um peso normal ele é: <br> ${imc.toFixed(2)}</h1>`;
         } else if (imc >= 25 && imc <= 29.9 ) {
             console.log("Você está com Sobrepeso");
-            resultado.innerHTML += `<h1>Você está com Sobrepeso ${imc.toFixed(2)}</h1>`;            
+            resultado.innerHTML += `<h1>Você está com Sobrepeso ele é: <br> ${imc.toFixed(2)}</h1>`;            
         } else if (imc >= 30 && imc <= 34.5 ) {
             console.log("Você está com Obsidade grau 1");
-            resultado.innerHTML += `<h1>Você está com Obsidade grau 1 ${imc.toFixed(2)}</h1>`; 
+            resultado.innerHTML += `<h1>Você está com Obsidade grau 1: <br> ${imc.toFixed(2)}</h1>`; 
         } else if (imc >= 35 && imc <= 39.9 ) {
             console.log("Você está com Obsidade grau 2");
-            resultado.innerHTML += `<h1>Você está com Obsidade grau 2 ${imc.toFixed(2)}</h1>`; 
+            resultado.innerHTML += `<h1>Você está com Obsidade grau 2 <br> ${imc.toFixed(2)}</h1>`; 
         } else if (imc >= 40){
             console.log("Você está com Obsidade grau 3");
-            resultado.innerHTML += `<h1>Você está com Obsidade grau 3 ${imc.toFixed(2)}</h1>`;
+            resultado.innerHTML += `<h1>Você está com Obsidade grau 3 <br> ${imc.toFixed(2)}</h1>`;
         } else{
             resultado.innerHTML += "Erro";
         }
