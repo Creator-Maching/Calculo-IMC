95ab3e270c6585fd6e7c3f501405ec2e8481cdcf
Pedro Dias Mendes Vianna <ordepmeta@gmail.com>
2025-04-09 12:52:24 -0300

    IMC

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+function envioDados(dados) {
+    const form = document.querySelector(".form");
+    const resultado = document.querySelector(".resultado");
+
+    function ativarEvento(evento){
+        evento.preventDefault()
+
+        const peso = Number(document.querySelector(".peso").value);
+        const altura = Number(document.querySelector(".altura").value);
+        const imc = peso / (altura * altura);
+
+          resultado.innerHTML = "";
+
+        if (imc < 18.5){
+            console.log("Você está abaixo do peso");
+            resultado.innerHTML += `<h1> Você está abaixo do peso ele é ${imc.toFixed(2)}</h1>`;
+        } else if (imc >= 18.5 && imc <= 24.9) {
+            console.log("Você tem um peso normal");
+            resultado.innerHTML +=`<h1>Você tem um peso normal ele é ${imc.toFixed(2)}</h1>`;
+        } else if (imc >= 25 && imc <= 29.9 ) {
+            console.log("Você está com Sobrepeso");
+            resultado.innerHTML += `<h1>Você está com Sobrepeso ${imc.toFixed(2)}</h1>`;            
+        } else if (imc >= 30 && imc <= 34.5 ) {
+            console.log("Você está com Obsidade grau 1");
+            resultado.innerHTML += `<h1>Você está com Obsidade grau 1 ${imc.toFixed(2)}</h1>`; 
+        } else if (imc >= 35 && imc <= 39.9 ) {
+            console.log("Você está com Obsidade grau 2");
+            resultado.innerHTML += `<h1>Você está com Obsidade grau 2 ${imc.toFixed(2)}</h1>`; 
+        } else if (imc >= 40){
+            console.log("Você está com Obsidade grau 3");
+            resultado.innerHTML += `<h1>Você está com Obsidade grau 3 ${imc.toFixed(2)}</h1>`;
+        } else{
+            resultado.innerHTML += "Erro";
+        }
+    }
+    form.addEventListener("submit", ativarEvento);
+}
+envioDados();
